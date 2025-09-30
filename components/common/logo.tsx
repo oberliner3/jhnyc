0fb75f4b593fb864626b13fc20bd42198bc74ee8
Mohamed Moughamir <moughamir@gmail.com>
2025-09-30 22:20:05 +0100

    Fix logo

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -4,7 +4,7 @@ import { SITE_CONFIG } from "@/lib/constants";
 export function Logo({ className = "" }: { className?: string }) {
 	return (
 		<Link href="/" className={`flex items-center space-x-2 ${className}`}>
-			<span className="font-bold text-xl">{SITE_CONFIG.name}</span>
+			<span className="font-bold text-xl">Vohovintage</span>
 		</Link>
 	);
 }
